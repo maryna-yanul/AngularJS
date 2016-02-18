2185f46d8980497f3c0d70a38560d47fc4127dcb
Maryna Yanul <maryna.yanul@gmail.com>
2016-02-18 16:22:29 +0200

    Properly Including Images With ngSrc

--- directory.js
+++ directory.js
@@ -5,10 +5,10 @@ angular.module('directoryApp', [])
 		dirList.toggle = false;
 
 		dirList.list = [
-		{name: 'Marina', age: 24},
-		{name: 'Steve', age: 16},
-		{name: 'Lo', age: 14},
-		{name: 'Sonya', age: 23}
+		{name: 'Marina', age: 24, img:'http://www.npg.org.uk/assets/microsites/bp2016/images/home_1.jpg'},
+		{name: 'Steve', age: 16, img:'http://www.fubiz.net/wp-content/uploads/2011/08/martin-schoeller-barack-obama-portrait.jpeg'},
+		{name: 'Lo', age: 14, img:'http://thehudgens.org/wp-content/uploads/2013/03/chambers-portrait-by-Jason-Travis.jpg'},
+		{name: 'Sonya', age: 23, img:'http://www.canvaz.com/portrait/charcoal-1.jpg'}
 		];
 
 		dirList.addPerson = function() {
