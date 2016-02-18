6008a9653b23cff670ae30631a752eb7ffd951df
Maryna Yanul <maryna.yanul@gmail.com>
2016-02-18 14:57:17 +0200

    Adding A Form Event

--- directory.js
+++ directory.js
@@ -5,5 +5,10 @@ angular.module('directoryApp', [])
 		{name: 'Stive', age: 16},
 		{name: 'Lo', age: 14},
 		{name: 'Sonya', age: 23}
-		   ]		
+		]	
+		$scope.addPerson = function() {
+			$scope.list.push({name:$scope.name, age:$scope.age});
+			$scope.name = '';
+			$scope.age = 0;
+		}	
 	});
